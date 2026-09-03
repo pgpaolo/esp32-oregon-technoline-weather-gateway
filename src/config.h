d5764f25ecf04965d09ed13af2695429d2f9f89e
Gianpaolo P. <68400366+pgpaolo@users.noreply.github.com>
2026-09-03 16:14:21 +0200

    Mark develop firmware as 6.4.0-dev1

--- src/config.h
+++ src/config.h
@@ -6,7 +6,7 @@
 #endif
 
 #ifndef FIRMWARE_VERSION
-#define FIRMWARE_VERSION         "6.4.0-rc3"
+#define FIRMWARE_VERSION         "6.4.0-dev1"
 #endif
 #ifndef GIT_COMMIT_HASH
 #define GIT_COMMIT_HASH          "source-archive"
